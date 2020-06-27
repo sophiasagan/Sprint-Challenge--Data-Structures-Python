6dfbaf62fe7ee7af017ae3be1591eecf6e48351b
Sophia Sagan <sophiabraddock82@gmail>
2020-06-26 20:02:06 -0400

    ring-buffer done and passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity): # initialize capacity
+        self.capacity = capacity
+        self.data = []
+        self.cur_index = 0
 
     def append(self, item):
-        pass
+        if len(self.data) >= self.capacity: # if data is longer than capacity
+            self.data[self.cur_index] = item #replace current node 
+            self.cur_index = (self.cur_index + 1) % self.capacity #increment index
+
+        else:
+            self.data.append(item) # append item at the end
 
     def get(self):
-        pass
+        return self.data # return items from oldest to newest
